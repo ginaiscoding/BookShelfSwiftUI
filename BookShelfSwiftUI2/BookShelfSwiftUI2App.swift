b63870a558d5719c86b6badc37716e4976316adc
ginaiscoding <rpaek88@gmail.com>
2022-07-07 14:25:05 -0700

    Initial Commit

---
+++ BookShelfSwiftUI2/BookShelfSwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfSwiftUI2App.swift
+//  BookShelfSwiftUI2
+//
+//  Created by Regina Paek on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct BookShelfSwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookShelfSwiftUI2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookShelfSwiftUI2
+//
+//  Created by Regina Paek on 7/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
