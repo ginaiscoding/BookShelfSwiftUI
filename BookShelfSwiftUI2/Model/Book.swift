142e3d7a159e894d9c56493d0c8fb56d09abd6c0
ginaiscoding <rpaek88@gmail.com>
2022-07-07 17:15:13 -0700

    Made a bookshelf app using SWIFTUI

---
+++ BookShelfSwiftUI2/Model/Book.swift
@@ -0,0 +1,8 @@
+//
+//  Book.swift
+//  BookShelfSwiftUI2
+//
+//  Created by Regina Paek on 7/7/22.
+//
+
+import Foundation
---
+++ BookShelfSwiftUI2/Views/BookDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  BookDetailView.swift
+//  BookShelfSwiftUI2
+//
+//  Created by Regina Paek on 7/7/22.
+//
+
+import SwiftUI
+
+struct BookDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BookDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookDetailView()
+    }
+}
